5afe20ed7ed07dba4731427ccfbf0906259d19f2
yfyeung <yifanyeung@sjtu.edu.cn>
2024-03-31 16:26:00 +0800

    add recipe for gigaspeech2

---
+++ lhotse/recipes/gigaspeech2.py
@@ -0,0 +1,152 @@
+import json
+import logging
+import os
+from collections import defaultdict
+from concurrent.futures.thread import ThreadPoolExecutor
+from pathlib import Path
+from typing import Dict, List, Optional, Sequence, Tuple, Union
+
+from tqdm.auto import tqdm
+
+from lhotse.audio import Recording, RecordingSet
+from lhotse.qa import fix_manifests, validate_recordings_and_supervisions
+from lhotse.recipes.utils import manifests_exist
+from lhotse.supervision import SupervisionSegment, SupervisionSet
+from lhotse.utils import Pathlike, add_durations
+
+
+def _parse_utterance(
+    part_path: Pathlike,
+    line: str,
+    lang: Optional[str] = None,
+) -> Optional[Tuple[Recording, SupervisionSegment]]:
+    recording_id, text = line.strip().split(maxsplit=1)
+    audio_path = (
+        part_path / recording_id.rsplit("-", maxsplit=1)[0] / f"{recording_id}.wav"
+    )
+
+    if not audio_path.is_file():
+        logging.warning(f"No such file: {audio_path}")
+        return None
+
+    recording = Recording.from_file(
+        path=audio_path,
+        recording_id=recording_id,
+    )
+
+    segment = SupervisionSegment(
+        id=recording_id,
+        recording_id=recording_id,
+        start=0.0,
+        duration=recording.duration,
+        channel=0,
+        language=lang,
+        text=text.strip(),
+    )
+
+    return recording, segment
+
+
+def _prepare_subset(
+    subset: str,
+    corpus_dir: Pathlike,
+    lang: Optional[str] = None,
+    num_jobs: int = 1,
+) -> Tuple[RecordingSet, SupervisionSet]:
+    """
+    Returns the RecodingSet and SupervisionSet given a dataset part.
+    :param subset: str, the name of the subset.
+    :param corpus_dir: Pathlike, the path of the data dir.
+    :return: the RecodingSet and SupervisionSet for train and valid.
+    """
+    corpus_dir = Path(corpus_dir)
+    part_path = corpus_dir / subset
+    trans_paths = list(part_path.rglob("*.trans.txt"))
+
+    with ThreadPoolExecutor(num_jobs) as ex:
+        futures = []
+        recordings = []
+        supervisions = []
+        for trans_path in tqdm(trans_paths, desc="Distributing tasks"):
+            with open(trans_path) as f:
+                for line in f:
+                    futures.append(ex.submit(_parse_utterance, part_path, line, lang))
+
+        for future in tqdm(futures, desc="Processing"):
+            result = future.result()
+            if result is None:
+                continue
+            recording, segment = result
+            recordings.append(recording)
+            supervisions.append(segment)
+
+        recording_set = RecordingSet.from_recordings(recordings)
+        supervision_set = SupervisionSet.from_segments(supervisions)
+
+        # Fix manifests
+        recording_set, supervision_set = fix_manifests(recording_set, supervision_set)
+        validate_recordings_and_supervisions(recording_set, supervision_set)
+
+    return recording_set, supervision_set
+
+
+def prepare_gigaspeech2(
+    corpus_dir: Pathlike,
+    output_dir: Optional[Pathlike] = None,
+    lang: Optional[str] = None,
+    num_jobs: int = 1,
+) -> Dict[str, Dict[str, Union[RecordingSet, SupervisionSet]]]:
+    """
+    Returns the manifests which consist of the Recordings and Supervisions
+    :param corpus_dir: Path to the Gigaspeech2 dataset.
+    :param output_dir: Pathlike, the path where to write the manifests.
+    :return: a Dict whose key is the dataset part, and the value is Dicts with the keys 'recordings' and 'supervisions'.
+    """
+    corpus_dir = Path(corpus_dir)
+
+    assert corpus_dir.is_dir(), f"No such directory: {corpus_dir}"
+
+    logging.info("Preparing Gigaspeech2...")
+
+    if output_dir is not None:
+        output_dir = Path(output_dir)
+        output_dir.mkdir(parents=True, exist_ok=True)
+
+    manifests = defaultdict(dict)
+
+    for part in tqdm(corpus_dir.iterdir(), desc="Dataset parts"):
+        part = part.stem
+        logging.info(f"Processing Gigaspeech2 subset: {part}")
+        if manifests_exist(
+            part=part,
+            output_dir=output_dir,
+            prefix="gigaspeech2",
+            suffix="jsonl.gz",
+        ):
+            logging.info(f"Gigaspeech2 subset: {part} already prepared - skipping.")
+            continue
+
+        recording_set, supervision_set = _prepare_subset(
+            part, corpus_dir, lang, num_jobs
+        )
+
+        if output_dir is not None:
+            supervision_set.to_file(
+                output_dir / f"gigaspeech2_supervisions_{part}.jsonl.gz"
+            )
+            recording_set.to_file(
+                output_dir / f"gigaspeech2_recordings_{part}.jsonl.gz"
+            )
+
+        manifests[part] = {"recordings": recording_set, "supervisions": supervision_set}
+
+    return manifests
+
+
+if __name__ == "__main__":
+    prepare_gigaspeech2(
+        corpus_dir="/mnt/cloudstorfs/sjtu_home/yifan.yang02/raw_data/GigaSpeech2",
+        output_dir="/mnt/cloudstorfs/sjtu_home/yifan.yang02/icefall_data/gigaspeech2/data/manifests",
+        lang="Thai",
+        num_jobs=16,
+    )
